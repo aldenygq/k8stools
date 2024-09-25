b1b74242c8eb2b982fd81f558b6322bb98499197
Alden <ygq13572549874@163.com>
2024-09-25 19:32:56 +0800

    Create ns.go
    Signed-off-by: Alden <ygq13572549874@163.com>

---
+++ ns.go
@@ -0,0 +1,11 @@
+func CreateNs(client *ns string) error {
+	// 创建namespace的spec
+	var namespace coreV1.Namespace
+	namespace.Name = ns
+	defer k.CloseClient()
+	_, err := k.Client.CoreV1().Namespaces().Create(context.TODO(),&namespace,metaV1.CreateOptions{})
+    if err != nil {
+        return err 
+    }
+	return nil 
+}

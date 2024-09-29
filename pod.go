c6ddcf17b9a74d8b68153addf52dd93c1497c79a
Alden <ygq13572549874@163.com>
2024-09-29 16:42:51 +0800

    Update pod.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- pod.go
+++ pod.go
@@ -3,6 +3,7 @@ import (
     "k8s.io/client-go/kubernetes"
     metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "context"
+    coreV1 "k8s.io/api/core/v1"
 )
 func DeletePod(c *kubernetes.Clientset,ns,podname string) error {
     err := c.CoreV1().Pods(ns).Delete(context.TODO(), podname, metaV1.DeleteOptions{})
@@ -10,4 +11,29 @@ func DeletePod(c *kubernetes.Clientset,ns,podname string) error {
         return err
     }
     return nil
+}
+func PodList(c *kubernetes.Clientset,ns string) (*coreV1.PodList,error) {
+    var (
+        podlist *coreV1.PodList = &coreV1.PodList{}
+        err error
+    )
+    // 列出Pods
+    if ns != "" {
+        podlist, err = c.CoreV1().Pods(ns).List(context.TODO(), metaV1.ListOptions{})
+    } else {
+        podlist, err = c.CoreV1().Pods("").List(context.TODO(), metaV1.ListOptions{})
+    }
+    if err != nil {
+        return nil,err
+    }
+    return podlist,nil
+}
+
+
+func PodInfo(c *kubernetes.Clientset,ns,podname string) (*coreV1.Pod,error) {
+    pod, err := c.CoreV1().Pods(ns).Get(context.TODO(), podname, metaV1.GetOptions{})
+    if err != nil {
+        return nil,err
+    }
+    return pod,nil
 }

094a915f01298a35d84b91e4fd85001942a89f94
Alden <ygq13572549874@163.com>
2024-09-30 16:29:25 +0800

    Update secret.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- secret.go
+++ secret.go
@@ -5,6 +5,7 @@ import (
         "context"
         metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
         "fmt"
+        "github.com/aldenygq/toolkits"
 )
 func UpdateSecretByOpaque(c *kubernetes.Clientset,ns,secretname string,kv map[string][]byte) error {
     secret := &coreV1.Secret{
@@ -111,4 +112,34 @@ func CreateSecretByImageCert(c *kubernetes.Clientset,ns,secretname,url,user,pass
      }
 
      return nil
+}
+func SecretInfo(c *kubernetes.Clientset,ns,sectet string) (*coreV1.Secret,error) {
+    secretinfo,err := c.CoreV1().Secrets(ns).Get(context.Background(),sectet,metaV1.GetOptions{})
+    if err != nil {
+        return nil,err
+    }
+    return secretinfo,nil
+}
+
+func SecretList(c *kubernetes.Clientset,ns string) (*coreV1.SecretList,error) {
+    var (
+        secrets *coreV1.SecretList = &coreV1.SecretList{}
+        err error
+    )
+    if ns != "" {
+        secrets,err = c.CoreV1().Secrets(ns).List(context.Background(),metaV1.ListOptions{})
+    } else {
+        secrets,err = c.CoreV1().Secrets("").List(context.Background(),metaV1.ListOptions{})
+    }
+    if err != nil {
+        return nil,err
+    }
+    return  secrets,nil
+}
+func DeleteSecret(c *kubernetes.Clientset,ns,name string) error {
+    err := c.CoreV1().Secrets(ns).Delete(context.TODO(), name, metaV1.DeleteOptions{GracePeriodSeconds: toolkits.Int64ToPointInt64(0)})
+    if err != nil {
+        return err
+    }
+    return nil
 }
